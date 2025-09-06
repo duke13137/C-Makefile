d9a3e74f38b0fcb228bfb99259a25c2bd84546c0
Duke <duke@home.com>
2025-09-06 14:17:19 -0400

    include/stc

--- src/adt.h
+++ src/adt.h
@@ -1,19 +1,14 @@
 // clang-format off
 #pragma once
 
-#include "arena.h"
-#include "datatype99.h"
 #include "interface99.h"
+#include "stc/algorithm.h"
 
-#define TREE(tree)                New(arena, BinaryTree, 1, (BinaryTree[]){tree})
-#define NODE(left, number, right) TREE(Node(left, number, right))
-#define LEAF(number)              TREE(Leaf(number))
+#include "arena.h"
+#include "utest.h"
 
-datatype(
-    BinaryTree,
-    (Leaf, int),
-    (Node, const BinaryTree *, int, const BinaryTree *)
-);
+#ifndef _CLANGD
+#include "datatype99.h"
 
 #define EXPR(expr)       New(arena, Expr, 1, ((Expr[]){expr}))
 #define OP(lhs, op, rhs) EXPR(op(EXPR(lhs), EXPR(rhs)))
@@ -27,11 +22,64 @@ datatype(
     (Div, Expr *, Expr *)
 );
 
+static double eval(const Expr *expr) {
+  match(*expr) {
+    of(Const, number) return *number;
+    of(Add, lhs, rhs) return eval(*lhs) + eval(*rhs);
+    of(Sub, lhs, rhs) return eval(*lhs) - eval(*rhs);
+    of(Mul, lhs, rhs) return eval(*lhs) * eval(*rhs);
+    of(Div, lhs, rhs) return eval(*lhs) / eval(*rhs);
+  }
 
-// clang-format off
+  Assert("Invalid expr");
+  return -1;
+}
+
+static Expr *expr(Arena *arena) {
+  return OP(*OP(*OP(Const(53), Add, Const(5)), Sub, Const(10)), Div, Const(8));
+}
+
+UTEST(datatype99, adt) {
+  byte mem[KB(1)] = {0};
+  Arena arena = arena_init(mem, KB(1));
+  ASSERT_EQ(6.0, eval(expr(&arena)));
+}
+
+#endif
+
 #define Shape_IFACE                      \
     vfunc( int, perim, const VSelf)      \
     vfunc(void, scale, VSelf, int factor)
-// clang-format on
 
 interface(Shape);
+
+#define TREE(tree)                New(arena, Tree, 1, &c_variant(Node, tree))
+#define NODE(left, number, right) TREE(Node(left, number, right))
+#define LEAF(number)              TREE(Leaf(number))
+
+c_union(Tree,
+  (Empty, bool),
+  (Leaf, int),
+  (Node, struct { int value; Tree *left, *right; }));
+
+static int tree_sum(Tree *t) {
+  c_when(t) {
+    c_is(Empty) return 0;
+    c_is(Leaf, v) return *v;
+    c_is(Node, n) return n->value + tree_sum(n->left) + tree_sum(n->right);
+  }
+  return -1;
+}
+
+UTEST(std, adt) {
+  Tree* tree =
+  &c_variant(Node, {1,
+      &c_variant(Node, {2,
+          &c_variant(Leaf, 3),
+          &c_variant(Leaf, 4)
+      }),
+      &c_variant(Leaf, 5)
+  });
+
+  ASSERT_EQ(15, tree_sum(tree));
+}
