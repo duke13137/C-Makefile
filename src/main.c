d87f1f3f27b38df5b5f7e4746dddaaa99f236867
Duke <duke@home.com>
2025-12-26 16:13:39 -0500

    improve Push/Clone slice

--- src/main.c
+++ src/main.c
@@ -146,8 +146,8 @@ i64s test_slice(Arena* arena) {
     Scratch(arena);
     slice(int) fibs = {0};
     fibs = Clone(arena, fibs);
-    *Push(&fibs, arena) = 2;
-    *Push(&fibs, arena) = 3;
+    *Push(arena, &fibs) = 2;
+    *Push(arena, &fibs) = 3;
   }
 
   int64_t data[] = {2, 3, 42};
@@ -156,7 +156,7 @@ i64s test_slice(Arena* arena) {
     Scratch(arena);
     fibs = Clone(arena, fibs, 0, 2);
     for (int i = 2; i < 9; ++i) {
-      *Push(&fibs, arena) = fibs.data[i - 2] + fibs.data[i - 1];
+      *Push(arena, &fibs) = fibs.data[i - 2] + fibs.data[i - 1];
     }
 
     ALOG(arena);
@@ -301,7 +301,7 @@ int main(int argc, const char* argv[]) {
 
   i64s fibs = test_slice(arena);
   fibs.cap = 0;
-  *Push(&fibs, arena) = 0;
+  *Push(arena, &fibs) = 0;
   puts(">>>fibs");
   for (int i = 0; i < fibs.len; ++i)
     printf("%ld,", fibs.data[i]);
